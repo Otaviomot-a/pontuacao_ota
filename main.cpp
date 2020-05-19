84b5d4ac8679290ee0105cd0780fd991baaf78d1
Otávio Augusto Mota <otavioamota4@gmail.com>
2020-05-19 10:25:39 -0300

    Classificação final

--- main.cpp
+++ main.cpp
@@ -10,14 +10,14 @@ int main(){
     cout << "INFORME OS DADOS DO ULTIMO ANO" << endl << endl;
 
     double compras, ticket, atrasos, pagar, mediav, pontuacao1, pontuacao2, pontuacao3;
-    string formadepagamento;
+    string formadpagar;
 //volume
     cout << "Quantas compras o cliente fez no ultimo ano? "; cin >> compras;
     cout << "Qual o ticket medio? "; cin >> ticket;
 //dados
     cout << endl << "Quantas vezes o cliente atrasou o pagamento? "; cin >> atrasos;
     cout << "A maioria das compras foi em dinheiro, cartao, ou boleto (D/C/B) "; cin >> pagar;
-//contas
+//score compras
     if (mediav == 0) {
         pontuacao1 = 0;
         }
@@ -30,9 +30,9 @@ int main(){
     else {
         pontuacao1 = 60;
         }
-//score compras
     cout << endl << "Score de volume de compras = " << pontuacao1 << " pontos" << endl;
 
+//inadimplecia
     if (atrasos > 1 || compras == 0){
         pontuacao2 = 0;
     }
@@ -44,5 +44,25 @@ int main(){
     }
 
     cout << "Score de idadimplecia = " << pontuacao2 << " pontos" << endl;
+
+    //pagamento
+
+    if (formadpagar == "D" && compras > 0){
+        pontuacao3 = 5;
+    }
+    if (formadpagar == "C" || formadpagar == "B" && compras > 0){
+        pontuacao3 = 10;
+    }
+    cout << "Score de forma de pagamento = " << pontuacao3 << " pontos" << endl;
+    if (pontuacao1 + pontuacao2 + pontuacao3 <= 25){
+        cout << endl << "Classificacao final = CLIENTE BRONZE" << endl;
+    }
+    else if (pontuacao1 + pontuacao2 + pontuacao3 < 75) {
+       cout << endl << "Classificacao final = CLIENTE PRATA" << endl;
+    }
+    if (pontuacao1 + pontuacao2 + pontuacao3 > 75){
+       cout << endl << "Classificacao final = CLIENTE OURO" << endl;
+    }
     return 0;
+
 }
