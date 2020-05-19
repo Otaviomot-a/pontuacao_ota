2109ab32c564bacda2a87eeb59c4d3ed5edb4e66
Otávio Augusto Mota <otavioamota4@gmail.com>
2020-05-19 09:15:13 -0300

    Leitura dos outros dados

--- main.cpp
+++ main.cpp
@@ -6,13 +6,15 @@ int main(){
 
     cout << endl << "SISTEMA DE PERFIL DE CLIENTE" << endl;
     cout << "--------------------------------" << endl;
-    cout << "INFORME OS DADOS DO ULTIMO ANO" << endl;
+    cout << "INFORME OS DADOS DO ULTIMO ANO" << endl << endl;
 
-    int compras, ticket;
+    double compras, ticket, atrasos, pagar;
 
-    cout << "Quantas compras o cliente fez no ultimo ano?"; cin >> compras;
-    cout << endl << "Qual o ticket medio?"; cin >> ticket;
+    cout << "Quantas compras o cliente fez no ultimo ano? "; cin >> compras;
+    cout << "Qual o ticket medio? "; cin >> ticket;
 
+    cout << endl << "Quantas vezes o cliente atrasou o pagamento? "; cin >> atrasos;
+    cout << "A maioria das compras foi em dinheiro, cartao, ou boleto (D/C/B) "; cin >> pagar;
 
     return 0;
 }
